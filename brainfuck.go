cf414cf671183434c99794a4ae5e40ab65b5069e
Glenn Franxman <gfranxman@gmail.com>
2009-11-21 03:49:05 -0500

    Switching (pun intended) to using switch statement instead of if/else construct.

--- brainfuck.go
+++ brainfuck.go
@@ -32,69 +32,63 @@ func main() {
         // execution loop
         for instruction_ptr = 0; instruction_ptr < len(program); instruction_ptr++ {
 
-            // '>' increment the data pointer (to point to the next cell to the right).
-            if program[instruction_ptr] == '>' {
-                data_ptr++;
-            }
+            switch program[instruction_ptr] {
 
-            // '<' decrement the data pointer (to point to the next cell to the left).
-            else if program[instruction_ptr] == '<' {
-                data_ptr--;
-            }
+                case '>': 
+                    // increment the data pointer (to point to the next cell to the right).
+                    data_ptr++;
+            
+                case '<': 
+                    // decrement the data pointer (to point to the next cell to the left).
+                    data_ptr--;
 
+                case '+': 
+                    // increment (increase by one) the byte at the data pointer.
+                    data[data_ptr]++;
 
-            // '+' increment (increase by one) the byte at the data pointer.
-            else if program[instruction_ptr] == '+' {
-                data[data_ptr]++;
-            }
+                case '-': 
+                    // decrement (decrease by one) the byte at the data pointer.
+                    data[data_ptr]--;
 
-            // '-' decrement (decrease by one) the byte at the data pointer.
-            else if program[instruction_ptr] == '-' {
-                data[data_ptr]--;
-            }
+                case '.': 
+                    // output the value of the byte at the data pointer.
+                    print( string( uint8(data[data_ptr]) ) ) ;
 
-            // '.' output the value of the byte at the data pointer.
-            else if program[instruction_ptr] == '.' {
-                print( string( uint8(data[data_ptr]) ) ) ;
-            }
+                case ',': 
+                    // accept one byte of input, storing its value in the byte at the data pointer.
+                    var b =  make( []uint8, 1 ) ;
+                    _,_ = os.Stdin.Read(b);
+                    data[data_ptr] = b[0];
 
-            // ',' accept one byte of input, storing its value in the byte at the data pointer.
-            else if program[instruction_ptr] == ',' {
-                var b =  make( []uint8, 1 ) ;
-                _,_ = os.Stdin.Read(b);
-                data[data_ptr] = b[0];
-            }
-
-            // '[' if the byte at the data pointer is zero, then 
-            //    instead of moving the instruction pointer forward to the next command, 
-            //    jump it forward to the command after the matching ] command.
-            // * interesting note -- the spec does not mention that the jump instructions should be nestable,
-            //    but without this feature my test suite fails.
-            else if program[instruction_ptr] == '[' {
-                if data[data_ptr] == 0 {
-                    instruction_ptr++;
-                    // allow nested [ ] pairs by looping until we hit the end-jump for our loop depth
-                    for loop_depth > 0 || program[instruction_ptr] != 93 {
-                        if program[instruction_ptr] == '[' { 
-                            loop_depth++; 
-                        }
-                        else if program[instruction_ptr] == ']' { 
-                            loop_depth--; 
-                        }
+                case '[': 
+                    // if the byte at the data pointer is zero, then 
+                    //    instead of moving the instruction pointer forward to the next command, 
+                    //    jump it forward to the command after the matching ] command.
+                    // * interesting note -- the spec does not mention that the jump instructions should be nestable,
+                    //    but without this feature my test suite fails.
+                    if data[data_ptr] == 0 {
                         instruction_ptr++;
+                        // allow nested [ ] pairs by looping until we hit the end-jump for our loop depth
+                        for loop_depth > 0 || program[instruction_ptr] != ']' {
+                            if program[instruction_ptr] == '[' { 
+                                loop_depth++; 
+                            }
+                            else if program[instruction_ptr] == ']' { 
+                                loop_depth--; 
+                            }
+                            instruction_ptr++;
+                        }
                     }
-                }
-            }
 
-            // ']' if the byte at the data pointer is nonzero, 
-            //    then instead of moving the instruction pointer forward to the next command, 
-            //    jump it back to the command after the matching [ command.
-            // * interesting note -- the spec calls for a check that the datapointer is pointing to a non-zero value,
-            //    but doing so causes my test suite of bf programs to fail.
-            else if program[instruction_ptr] == ']' {
-                instruction_ptr--;
-                // allow nested [ ] pairs by looping until we hit the end-jump for our loop depth
-                for loop_depth > 0 || program[instruction_ptr] != '[' {
+                case ']': 
+                    // if the byte at the data pointer is nonzero, 
+                    //    then instead of moving the instruction pointer forward to the next command, 
+                    //    jump it back to the command after the matching [ command.
+                    // * interesting note -- the spec calls for a check that the datapointer is pointing to a non-zero value,
+                    //    but doing so causes my test suite of bf programs to fail.
+                    instruction_ptr--;
+                    // allow nested [ ] pairs by looping until we hit the end-jump for our loop depth
+                    for loop_depth > 0 || program[instruction_ptr] != '[' {
                         if program[instruction_ptr] == ']' { 
                             loop_depth++; 
                         }
@@ -102,9 +96,9 @@ func main() {
                             loop_depth--; 
                         }
                         instruction_ptr--;
-                }
-                instruction_ptr--;
-            }
+                    }
+                    instruction_ptr--;
+            }// end-switch
 
         }  
     print("\n");  
